8a3cff09e2967204948171ed48a260ece1fbc310
Joseph Rock <josephrockqz@gmail.com>
2023-12-28 01:14:32 -0500

    feat: unsuccessfully add cors middleware

--- internal/app/app.go
+++ internal/app/app.go
@@ -3,11 +3,17 @@ package app
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/josephrockqz/wemusic-golang/internal/services"
+	"github.com/josephrockqz/wemusic-golang/internal/transport/middleware"
 )
 
 func Run() {
 	router := gin.Default()
 
+	// router.SetTrustedProxies([]string{"127.0.0.1"})
+
+	router.Use(middleware.CorsMiddleware())
+
+	router.GET("/spotify-login", services.SpotifyLogin)
 	router.GET("/spotify-user-authorization-callback", services.SpotifyUserAuthorizationCallback)
 
 	router.Run("localhost:8080")
