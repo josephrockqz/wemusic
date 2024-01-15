2a4ef5315ea633ae0ce8efdde1ec6fc233a01c79
Joseph Rock <josephrockqz@gmail.com>
2024-01-15 13:40:04 -0500

    feat: add to CORS middleware

--- internal/transport/middleware/cors.go
+++ internal/transport/middleware/cors.go
@@ -8,6 +8,20 @@ import (
 func CorsMiddleware() echo.MiddlewareFunc {
 	return middleware.CORSWithConfig(middleware.CORSConfig{
 		AllowOrigins: []string{"http://localhost:3000"},
-		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
+		AllowHeaders: []string{
+			echo.HeaderOrigin,
+			echo.HeaderXRequestedWith,
+			echo.HeaderContentType,
+			echo.HeaderAccept,
+			echo.HeaderAuthorization,
+		},
+		AllowMethods: []string{
+			echo.GET,
+			echo.HEAD,
+			echo.OPTIONS,
+			echo.POST,
+			echo.PUT,
+		},
+		AllowCredentials: true,
 	})
 }
