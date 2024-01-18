cbbb064b2ea20b28bbed24b8811e96754be77ef0
Joseph Rock <josephrockqz@gmail.com>
2024-01-17 22:16:08 -0500

    feat: change custom logger impl

--- go.mod
+++ go.mod
@@ -2,10 +2,7 @@ module github.com/josephrockqz/wemusic-golang
 
 go 1.20
 
-require (
-	github.com/labstack/echo/v4 v4.11.4
-	go.uber.org/zap v1.26.0
-)
+require github.com/labstack/echo/v4 v4.11.4
 
 require golang.org/x/crypto v0.17.0 // indirect
 
@@ -16,7 +13,6 @@ require (
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
-	go.uber.org/multierr v1.11.0 // indirect
 	golang.org/x/net v0.19.0 // indirect
 	golang.org/x/sys v0.15.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
--- internal/transport/middleware/logger.go
+++ internal/transport/middleware/logger.go
@@ -3,25 +3,24 @@ package middleware
 import (
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
-	"go.uber.org/zap"
 )
 
 func LoggerMiddleware() echo.MiddlewareFunc {
 
-	logger, _ := zap.NewProduction()
+	return middleware.LoggerWithConfig(middleware.LoggerConfig{
+		/*
+			fields removed from default config:
 
-	return middleware.RequestLoggerWithConfig(middleware.RequestLoggerConfig{
-		LogMethod: true,
-		LogURI:    true,
-		LogStatus: true,
-		LogValuesFunc: func(c echo.Context, v middleware.RequestLoggerValues) error {
-			logger.Info("request",
-				zap.String("Method", v.Method),
-				zap.String("URI", v.URI),
-				zap.Int("status", v.Status),
-			)
-
-			return nil
-		},
+				"remote_ip":"${remote_ip}"
+				"user_agent":"${user_agent}"
+				"latency":${latency}
+				"latency_human":"${latency_human}"
+				"bytes_in":${bytes_in}
+				"bytes_out":${bytes_out}
+		*/
+		Format: `{"time":"${time_rfc3339_nano}","id":"${id}",` +
+			`"host":"${host}","method":"${method}","uri":"${uri}",` +
+			`"status":${status},"error":"${error}}"` + "\n",
+		CustomTimeFormat: "2006-01-02 15:04:05.00000",
 	})
 }
